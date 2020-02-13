b383441a1c75adb860679154e33fbd31f8f78c8f
Adlan <adlanyandarbiev@gmail.com>
2020-02-13 08:51:45 -0800

    Section 1

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,9 +1,19 @@
+//import React and React.DOM libraries;
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
+//create a react component;
+
+// const App = function() {
+//     return <div>Hi There!</div>;
+// }; ===> function based component
+
+const App = () => {
+    return <div>Hi There!</div>;
+};
+
+//take the react component and show it on the screen;
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
