2626014ef4e30aaffd65b3a615ec3deb41a486ea
Adlan <adlanyandarbiev@gmail.com>
2020-02-13 10:40:31 -0800

    Converting styling to JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -9,7 +9,13 @@ import ReactDOM from 'react-dom';
 // }; ===> function based component
 
 const App = () => {
-    return <div>Hi There!</div>;
+    return (
+        <div>
+            <label className="label" htmlFor="name" > Enter Name: </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        </div>
+    );
 };
 
 //take the react component and show it on the screen;
