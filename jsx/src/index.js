dcbdb91782b205251a02a46711bdb785fa5f712e
Adlan <adlanyandarbiev@gmail.com>
2020-02-14 10:31:56 -0800

    Start components section

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -20,9 +20,9 @@ const App = () => {
             <label className="label" htmlFor="name" > 
             { labelText } 
             </label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>
-            {buttonText.text}
+                <input id="name" type="text" />
+                    <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                    {buttonText.text}
             </button>
         </div>
     );
