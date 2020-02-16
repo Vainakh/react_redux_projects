365e67e1a91979f3961d743502450f8ca2dd3c1f
Adlan <adlanyandarbiev@gmail.com>
2020-02-15 20:55:45 -0800

    Handling errors

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: ''};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );  
-        return <div>Latitude:</div>;
+          position => { 
+              this.setState({ lat: position.coords.latitude });
+          },
+          err => {
+              this.setState({ errorMessage: err.message });
+          }
+        );
     }
-};
+    render() {
+          
+        return (
+        <div>
+            Latitude: {this.state.lat}
+            <br/>
+            Error: { this.state.errorMessage}
+        </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App/>,
