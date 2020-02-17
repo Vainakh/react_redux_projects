88d2d452fadb2055fbf0aefa175a851dfc7511d4
Adlan <adlanyandarbiev@gmail.com>
2020-02-16 17:28:49 -0800

    Passing state as props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
@@ -16,13 +16,14 @@ class App extends React.Component {
 
     render() {
           
-        if ( !this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: { this.state.lat }</div>;
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage } </div>;
         }
 
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Latitude: {this.state.errorMessage}</div>;
-        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        } 
+
         return <div>Loading...</div>;
     }
 }
