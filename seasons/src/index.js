d7a03cbbb1a95db4feda18781c872fb4887d36c8
Adlan <adlanyandarbiev@gmail.com>
2020-02-16 17:07:53 -0800

    Alternate state initialization

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' }; 
-    }
+    
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
