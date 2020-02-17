f1f406496155c80f786a8bd891f6ffe6a289e81e
Adlan <adlanyandarbiev@gmail.com>
2020-02-16 18:16:50 -0800

    Displaying icons from semantic ui

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -11,13 +11,17 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
     
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === "winter" ? "Burrr, it is chilly" : "Lets hit the beach!";
+    const text = season === "winter" ? 'Burrr, it is chilly' : 'Lets hit the beach!';
+    const icon = season === "winter" ? 'snowflake' : 'sun';
+
 
     return (
-        <div>
-            <h1>{text}</h1> 
-        </div>
-    )
+      <div>
+        <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={`${icon} icon`} />
+      </div>
+    );
 };
 
 export default SeasonDisplay;
