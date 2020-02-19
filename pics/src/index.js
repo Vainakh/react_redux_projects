23b6f4205b369fb0271c0fafb676d6af5a8bb44c
Adlan <adlanyandarbiev@gmail.com>
2020-02-19 14:36:08 -0800

    Starting pics project

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from'react-dom';
-import App from './components/App';
-
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-    <App/>, 
-    document.querySelector('#root')
+  <App />,
+  document.getElementById('root')
 );
