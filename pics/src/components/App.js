bc2e931d827ba526d8ca50b57b45a7aa262609de
Adlan <adlanyandarbiev@gmail.com>
2020-02-20 09:06:31 -0800

    Handling requests with async await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,26 +4,26 @@ import SearchBar from "./SearchBar";
 
 
 class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 511f799d65b6cb8a7e7bf21ff5197b9cc8d122bb6873275fe6c9ac8fa4dec43c"
+      }
+    });
+    console.log(response.data.results);
+  }
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-          params: { query: term },
-          headers: {
-            Authorization:
-              'Client-ID 511f799d65b6cb8a7e7bf21ff5197b9cc8d122bb6873275fe6c9ac8fa4dec43c'
-          }
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="ui container" style={{ marginTop: '10px' }}>
-                    <SearchBar onSubmit={this.onSearchSubmit}/>
-                </div>
-            </div>
-        );
-    }  
+  render() {
+    return (
+      <div>
+        <div className="ui container" style={{ marginTop: "10px" }}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
