bcc67f1ef3b7433951cad8254b0b29c0de87cf09
Adlan <adlanyandarbiev@gmail.com>
2020-02-20 12:25:27 -0800

    Rendering list components

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
 import unsplash from '../api/unsplash';
-import SearchBar from "./SearchBar";
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 
 class App extends React.Component {
@@ -20,7 +21,7 @@ class App extends React.Component {
       <div>
         <div className="ui container" style={{ marginTop: "10px" }}>
           <SearchBar onSubmit={this.onSearchSubmit} />
-          Found: {this.state.images.length} images
+          <ImageList images={this.state.images} />
         </div>
       </div>
     );
