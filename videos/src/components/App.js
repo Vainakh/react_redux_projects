27c980954cc1b74322363c87661bb90db22d5b16
Adlan <adlanyandarbiev@gmail.com>
2020-02-21 14:49:49 -0800

    Implementing youtube api and get video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
     onTermSubmit = term => {
-        console.log(term);
+
+       youtube.get('/search', {
+           params: {
+               q: term 
+           }
+       });
     };
 
     render() {
