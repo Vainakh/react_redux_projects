09f3bcae6a828cda65a6de72d522edd5049fc2f7
Adlan <adlanyandarbiev@gmail.com>
2020-02-28 06:41:26 -0800

    Quick refactiring and wrapping up the app

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,14 @@
 import _ from "lodash";
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
-    console.log("about to fetch posts");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log("fetched posts");
+    
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
@@ -13,12 +17,10 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });  
 };
 
-export const fetchUser = id =>  dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
 
 
