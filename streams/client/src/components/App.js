82aab0be95bb7cb97d871b7eead60ebb0bcce953
Adlan <adlanyandarbiev@gmail.com>
2020-03-01 09:27:37 -0800

    Component Scaffolding

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>
+        PageOne
+        <Link to="/pagetwo">Navigate to page two</Link>
+    </div>;
+};
+
+const PageTwo = () => {
+  return <div>
+      PageTwo
+      <Link to="/">Navigate to page one</Link>
+        <button>Click!</button>
+    </div>;
+};
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pageTwo" component={PageTwo} />
+                </div>   
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
